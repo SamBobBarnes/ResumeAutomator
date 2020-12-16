3fc38dd954607a9516baff598142b260d93683d7
SamBobBarnes <54638834+SamBobBarnes@users.noreply.github.com>
2020-12-16 12:55:22 -0600

    added try catch loop to make a new file when current file is open

--- ResumeAutomator/Main.cs
+++ ResumeAutomator/Main.cs
@@ -24,14 +24,38 @@ namespace ResumeAutomator
         {
             // Documentation @ https://afterlogic.com/mailbee-net/docs-itextsharp/
             Document doc = new Document(iTextSharp.text.PageSize.LETTER, 10, 10, 42, 35);
-            PdfWriter wri = PdfWriter.GetInstance(doc, new FileStream("Test.pdf", FileMode.Create));
+            String docname = "Test";
+            int numretrys = 0;
+            bool tryreadingfile = true;
+            do
+            {
+                try
+                {
+                    if (numretrys == 0)
+                    {
+                        PdfWriter wri = PdfWriter.GetInstance(doc, new FileStream(docname + ".pdf", FileMode.Create));
+                        tryreadingfile = false;
+                    }
+                    else
+                    {
+                        numretrys += 1;
+                        PdfWriter wri = PdfWriter.GetInstance(doc, new FileStream(docname + numretrys.ToString() + ".pdf", FileMode.Create));
+                        tryreadingfile = false;
+                    }
+                }
+                catch (IOException ex)
+                {
+                    Console.WriteLine("Please close the pdf file that is open");
+                }
+            } while (tryreadingfile);
+            
             doc.Open();// Open Document to write
-            // write some content
+                       // write some content
             Paragraph paragraph = new Paragraph("This is my first time using Paragraph! \n hi hello");
             //Now add the above created text using different class object to our pdf document
             doc.Add(paragraph);
             doc.Close(); //Close Document
-            
+
         }
     }
 }
