a66c2779e4404be4e7500440aa3b451dd629af5d
SamBobBarnes <54638834+SamBobBarnes@users.noreply.github.com>
2020-12-16 12:04:18 -0600

    added basic pdf creator code to button click

--- ResumeAutomator/Main.cs
+++ ResumeAutomator/Main.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
 
 namespace ResumeAutomator
 {
@@ -19,7 +22,15 @@ namespace ResumeAutomator
 
         private void createResume_Click(object sender, EventArgs e)
         {
-
+            Document doc = new Document(iTextSharp.text.PageSize.LETTER, 10, 10, 42, 35);
+            PdfWriter wri = PdfWriter.GetInstance(doc, new FileStream("Test.pdf", FileMode.Create));
+            doc.Open();// Open Document to write
+            // write some content
+            Paragraph paragraph = new Paragraph("This is my first time using Paragraph! \n hi hello");
+            //Now add the above created text using different class object to our pdf document
+            doc.Add(paragraph);
+            doc.Close(); //Close Document
+            
         }
     }
 }
