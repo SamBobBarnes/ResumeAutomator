50ad4846b2c371bec4a93c46392f5262847f031a
Samuel Barnes <tvsamuel444@gmail.com>
2020-12-24 21:32:01 -0500

    text boxes and buttons generate and line up add buttons dissapear unless last

--- ResumeAutomator/Skills.Designer.cs
+++ ResumeAutomator/Skills.Designer.cs
@@ -1,4 +1,7 @@
 ﻿
+using System.Collections.Generic;
+using System.Windows.Forms;
+
 namespace ResumeAutomator
 {
     partial class Skills
@@ -30,68 +33,33 @@ namespace ResumeAutomator
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Skills));
-            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.DeleteBtn1 = new System.Windows.Forms.Button();
-            this.AddButton1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // vScrollBar1
-            // 
-            this.vScrollBar1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.vScrollBar1.Location = new System.Drawing.Point(490, 0);
-            this.vScrollBar1.Name = "vScrollBar1";
-            this.vScrollBar1.Size = new System.Drawing.Size(34, 479);
-            this.vScrollBar1.TabIndex = 0;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(401, 31);
-            this.textBox1.TabIndex = 1;
-            // 
-            // DeleteBtn1
-            // 
-            this.DeleteBtn1.Location = new System.Drawing.Point(419, 12);
-            this.DeleteBtn1.Name = "DeleteBtn1";
-            this.DeleteBtn1.Size = new System.Drawing.Size(31, 31);
-            this.DeleteBtn1.TabIndex = 2;
-            this.DeleteBtn1.Text = "-";
-            this.DeleteBtn1.UseVisualStyleBackColor = true;
-            // 
-            // AddButton1
-            // 
-            this.AddButton1.Location = new System.Drawing.Point(456, 12);
-            this.AddButton1.Name = "AddButton1";
-            this.AddButton1.Size = new System.Drawing.Size(31, 31);
-            this.AddButton1.TabIndex = 3;
-            this.AddButton1.Text = "+";
-            this.AddButton1.UseVisualStyleBackColor = true;
             // 
             // Skills
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(524, 479);
-            this.Controls.Add(this.AddButton1);
-            this.Controls.Add(this.DeleteBtn1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.vScrollBar1);
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(314, 291);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Skills";
             this.Text = "Skills";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Skills_FormClosing);
             this.Load += new System.EventHandler(this.Skills_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.VScrollBar vScrollBar1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button DeleteBtn1;
-        private System.Windows.Forms.Button AddButton1;
+        //private System.Windows.Forms.TextBox SkillBox1;
+        //private System.Windows.Forms.Button DeleteBtn1;
+        //private System.Windows.Forms.Button AddButton1;
+        private List<TextBox> SkillBoxList = new List<TextBox> { };
+        private List<Button> DeleteBtnList = new List<Button> { };
+        private List<Button> AddButtonList = new List<Button> { };
     }
 }
--- ResumeAutomator/Skills.cs
+++ ResumeAutomator/Skills.cs
@@ -12,12 +12,121 @@ namespace ResumeAutomator
 {
     public partial class Skills : Form
     {
+        private int lowestBox = 6;
+        private int boxDiff = 26;
+        private int tabTracker = 0;
+
         public Skills()
         {
             InitializeComponent();
         }
 
         private void Skills_Load(object sender, EventArgs e)
+        {
+            // Add First Text Box
+            SkillBoxList.Add(new TextBox());
+            SkillBoxList[0].Location = new System.Drawing.Point(6, 6);
+            SkillBoxList[0].Size = new System.Drawing.Size(238, 20);
+            SkillBoxList[0].TabIndex = tabTracker;
+            this.Controls.Add(SkillBoxList[SkillBoxList.Count - 1]);
+            tabTracker += 1;
+            
+
+            // Add First Delete Button
+            DeleteBtnList.Add(new Button());
+            DeleteBtnList[0].Location = new System.Drawing.Point(248, 6);
+            DeleteBtnList[0].Size = new System.Drawing.Size(20, 20);
+            DeleteBtnList[0].Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            DeleteBtnList[0].Text = "-";
+            DeleteBtnList[0].UseVisualStyleBackColor = true;
+            DeleteBtnList[0].Click += new System.EventHandler(this.DeleteBtn_Click);
+            DeleteBtnList[0].TabIndex = tabTracker;
+            DeleteBtnList[0].Enabled = false;
+            this.Controls.Add(DeleteBtnList[DeleteBtnList.Count - 1]);
+            tabTracker += 1;
+
+            // Add First Add Button
+            AddButtonList.Add(new Button());
+            AddButtonList[0].Location = new System.Drawing.Point(272, 6);
+            AddButtonList[0].Size = new System.Drawing.Size(20, 20);
+            AddButtonList[0].Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            AddButtonList[0].Text = "+";
+            AddButtonList[0].UseVisualStyleBackColor = true;
+            AddButtonList[0].Click += new System.EventHandler(this.AddButton_Click);
+            AddButtonList[0].TabIndex = tabTracker;
+            this.Controls.Add(AddButtonList[AddButtonList.Count - 1]);
+            tabTracker += 1;
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            lowestBox += boxDiff;
+
+            // Add next Text Box
+            int index = SkillBoxList.Count;
+            SkillBoxList.Add(new TextBox());
+            SkillBoxList[index].Location = new System.Drawing.Point(6, lowestBox);
+            SkillBoxList[index].Size = new System.Drawing.Size(238, 20);
+            SkillBoxList[index].TabIndex = tabTracker;
+            this.Controls.Add(SkillBoxList[index]);
+            tabTracker += 1;
+
+            // Add next Delete Button
+            index = DeleteBtnList.Count;
+            DeleteBtnList.Add(new Button());
+            DeleteBtnList[index].Location = new System.Drawing.Point(248, lowestBox);
+            DeleteBtnList[index].Size = new System.Drawing.Size(20, 20);
+            DeleteBtnList[index].Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            DeleteBtnList[index].Text = "-";
+            DeleteBtnList[index].UseVisualStyleBackColor = true;
+            DeleteBtnList[index].TabIndex = tabTracker;
+            DeleteBtnList[index].Click += new System.EventHandler(this.DeleteBtn_Click);
+            this.Controls.Add(DeleteBtnList[index]);
+            tabTracker += 1;
+
+
+            // Add next Add Button
+            index = AddButtonList.Count;
+            AddButtonList.Add(new Button());
+            AddButtonList[index].Location = new System.Drawing.Point(272, lowestBox);
+            AddButtonList[index].Size = new System.Drawing.Size(20, 20);
+            AddButtonList[index].Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            AddButtonList[index].Text = "+";
+            AddButtonList[index].UseVisualStyleBackColor = true;
+            AddButtonList[index].Click += new System.EventHandler(this.AddButton_Click);
+            AddButtonList[index].TabIndex = tabTracker;
+            this.Controls.Add(AddButtonList[index]);
+            tabTracker += 1;
+
+            // Disable Add Buttons
+            foreach (Button but in AddButtonList) {
+                this.Controls.Remove(but);
+            }
+            AddButtonList = DisableUnlessLast(AddButtonList);
+            foreach (Button but in AddButtonList)
+            {
+                this.Controls.Add(but);
+            }
+
+        }
+
+        private void DeleteBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private List<Button> DisableUnlessLast(List<Button> buts)
+        {
+            int lastIndex = buts.Count - 1;
+            foreach (Button but in buts)
+            {
+                but.Visible = false;
+            }
+            buts[lastIndex].Visible = true;
+            return buts;
+        }
+
+        private void Skills_FormClosing(object sender, FormClosingEventArgs e)
         {
 
         }
