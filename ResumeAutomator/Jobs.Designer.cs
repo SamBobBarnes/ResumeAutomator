89a1c36b5b62c8cd8516aeabbaa7bc13ed16ef46
Samuel Barnes <tvsamuel444@gmail.com>
2021-02-09 12:38:58 -0600

    working on jobs page delete bug

--- ResumeAutomator/Jobs.Designer.cs
+++ ResumeAutomator/Jobs.Designer.cs
@@ -33,8 +33,8 @@ namespace ResumeAutomator
             this.addBtn = new System.Windows.Forms.Button();
             this.delBtn = new System.Windows.Forms.Button();
             this.jobBox = new System.Windows.Forms.ListBox();
-            this.downBtn = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.moveDownBtn = new System.Windows.Forms.Button();
+            this.moveUpBtn = new System.Windows.Forms.Button();
             this.titleBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.jobTitleBox = new System.Windows.Forms.TextBox();
@@ -48,6 +48,13 @@ namespace ResumeAutomator
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
+            this.dutiesBox = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.locBox = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.currentCheck = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.startMonth)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.endMonth)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.startYear)).BeginInit();
@@ -63,6 +70,7 @@ namespace ResumeAutomator
             this.addBtn.TabIndex = 0;
             this.addBtn.Text = "Add";
             this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
             // 
             // delBtn
             // 
@@ -73,6 +81,7 @@ namespace ResumeAutomator
             this.delBtn.TabIndex = 1;
             this.delBtn.Text = "Delete";
             this.delBtn.UseVisualStyleBackColor = true;
+            this.delBtn.Click += new System.EventHandler(this.delBtn_Click);
             // 
             // jobBox
             // 
@@ -83,23 +92,25 @@ namespace ResumeAutomator
             this.jobBox.TabIndex = 2;
             this.jobBox.SelectedIndexChanged += new System.EventHandler(this.jobBox_SelectedIndexChanged);
             // 
-            // downBtn
+            // moveDownBtn
             // 
-            this.downBtn.Location = new System.Drawing.Point(218, 41);
-            this.downBtn.Name = "downBtn";
-            this.downBtn.Size = new System.Drawing.Size(75, 23);
-            this.downBtn.TabIndex = 3;
-            this.downBtn.Text = "V";
-            this.downBtn.UseVisualStyleBackColor = true;
+            this.moveDownBtn.Location = new System.Drawing.Point(218, 41);
+            this.moveDownBtn.Name = "moveDownBtn";
+            this.moveDownBtn.Size = new System.Drawing.Size(75, 23);
+            this.moveDownBtn.TabIndex = 3;
+            this.moveDownBtn.Text = "V";
+            this.moveDownBtn.UseVisualStyleBackColor = true;
+            this.moveDownBtn.Click += new System.EventHandler(this.moveDownBtn_Click);
             // 
-            // button4
+            // moveUpBtn
             // 
-            this.button4.Location = new System.Drawing.Point(218, 12);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "Ʌ";
-            this.button4.UseVisualStyleBackColor = true;
+            this.moveUpBtn.Location = new System.Drawing.Point(218, 12);
+            this.moveUpBtn.Name = "moveUpBtn";
+            this.moveUpBtn.Size = new System.Drawing.Size(75, 23);
+            this.moveUpBtn.TabIndex = 4;
+            this.moveUpBtn.Text = "Ʌ";
+            this.moveUpBtn.UseVisualStyleBackColor = true;
+            this.moveUpBtn.Click += new System.EventHandler(this.moveUpBtn_Click);
             // 
             // titleBox
             // 
@@ -138,7 +149,7 @@ namespace ResumeAutomator
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(425, 66);
+            this.label3.Location = new System.Drawing.Point(304, 66);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(58, 13);
             this.label3.TabIndex = 9;
@@ -147,7 +158,7 @@ namespace ResumeAutomator
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(611, 66);
+            this.label4.Location = new System.Drawing.Point(490, 66);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(55, 13);
             this.label4.TabIndex = 10;
@@ -155,7 +166,7 @@ namespace ResumeAutomator
             // 
             // startMonth
             // 
-            this.startMonth.Location = new System.Drawing.Point(489, 64);
+            this.startMonth.Location = new System.Drawing.Point(368, 64);
             this.startMonth.Maximum = new decimal(new int[] {
             12,
             0,
@@ -178,7 +189,7 @@ namespace ResumeAutomator
             // 
             // endMonth
             // 
-            this.endMonth.Location = new System.Drawing.Point(672, 64);
+            this.endMonth.Location = new System.Drawing.Point(551, 64);
             this.endMonth.Maximum = new decimal(new int[] {
             12,
             0,
@@ -201,7 +212,7 @@ namespace ResumeAutomator
             // 
             // startYear
             // 
-            this.startYear.Location = new System.Drawing.Point(545, 64);
+            this.startYear.Location = new System.Drawing.Point(424, 64);
             this.startYear.Maximum = new decimal(new int[] {
             2200,
             0,
@@ -224,7 +235,7 @@ namespace ResumeAutomator
             // 
             // endYear
             // 
-            this.endYear.Location = new System.Drawing.Point(728, 64);
+            this.endYear.Location = new System.Drawing.Point(607, 64);
             this.endYear.Maximum = new decimal(new int[] {
             2200,
             0,
@@ -248,7 +259,7 @@ namespace ResumeAutomator
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(710, 66);
+            this.label5.Location = new System.Drawing.Point(589, 66);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(12, 13);
             this.label5.TabIndex = 15;
@@ -257,7 +268,7 @@ namespace ResumeAutomator
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(527, 66);
+            this.label6.Location = new System.Drawing.Point(406, 66);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(12, 13);
             this.label6.TabIndex = 16;
@@ -266,17 +277,88 @@ namespace ResumeAutomator
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(595, 66);
+            this.label7.Location = new System.Drawing.Point(474, 66);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(10, 13);
             this.label7.TabIndex = 17;
             this.label7.Text = "-";
+            // 
+            // dutiesBox
+            // 
+            this.dutiesBox.Location = new System.Drawing.Point(368, 116);
+            this.dutiesBox.Multiline = true;
+            this.dutiesBox.Name = "dutiesBox";
+            this.dutiesBox.Size = new System.Drawing.Size(404, 225);
+            this.dutiesBox.TabIndex = 18;
+            this.dutiesBox.TextChanged += new System.EventHandler(this.dutiesBox_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(293, 119);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(69, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Work Duties:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(365, 344);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(193, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Each line will be a separate bullet point.";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(697, 389);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(75, 13);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "Save on close";
+            // 
+            // locBox
+            // 
+            this.locBox.Location = new System.Drawing.Point(368, 90);
+            this.locBox.Name = "locBox";
+            this.locBox.Size = new System.Drawing.Size(404, 20);
+            this.locBox.TabIndex = 22;
+            this.locBox.TextChanged += new System.EventHandler(this.locBox_TextChanged);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(311, 93);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(51, 13);
+            this.label11.TabIndex = 23;
+            this.label11.Text = "Location:";
+            // 
+            // currentCheck
+            // 
+            this.currentCheck.AutoSize = true;
+            this.currentCheck.Location = new System.Drawing.Point(692, 65);
+            this.currentCheck.Name = "currentCheck";
+            this.currentCheck.Size = new System.Drawing.Size(80, 17);
+            this.currentCheck.TabIndex = 25;
+            this.currentCheck.Text = "Current Job";
+            this.currentCheck.UseVisualStyleBackColor = true;
+            this.currentCheck.CheckedChanged += new System.EventHandler(this.currentCheck_CheckedChanged);
             // 
             // Jobs
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 411);
+            this.Controls.Add(this.currentCheck);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.locBox);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.dutiesBox);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -290,8 +372,8 @@ namespace ResumeAutomator
             this.Controls.Add(this.jobTitleBox);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.titleBox);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.downBtn);
+            this.Controls.Add(this.moveUpBtn);
+            this.Controls.Add(this.moveDownBtn);
             this.Controls.Add(this.jobBox);
             this.Controls.Add(this.delBtn);
             this.Controls.Add(this.addBtn);
@@ -318,8 +400,8 @@ namespace ResumeAutomator
         private System.Windows.Forms.Button addBtn;
         private System.Windows.Forms.Button delBtn;
         private System.Windows.Forms.ListBox jobBox;
-        private System.Windows.Forms.Button downBtn;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button moveDownBtn;
+        private System.Windows.Forms.Button moveUpBtn;
         private System.Windows.Forms.TextBox titleBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox jobTitleBox;
@@ -333,5 +415,12 @@ namespace ResumeAutomator
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox dutiesBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox locBox;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.CheckBox currentCheck;
     }
 }
--- ResumeAutomator/Jobs.cs
+++ ResumeAutomator/Jobs.cs
@@ -21,6 +21,19 @@ namespace ResumeAutomator
         private JSONHandler jhandle;
         private int listIndex;
         private bool allowUpdate = false;
+        private Dictionary<string, string> newDict = new Dictionary<string, string>()
+                {
+                    {"LISTTITLE","" },
+                    {"TITLE",""},
+                    {"JOBTITLE",""},
+                    {"STARTMONTH",""},
+                    {"STARTYEAR",""},
+                    {"ENDMONTH",""},
+                    {"ENDYEAR",""},
+                    {"LOC",""},
+                    {"DUTIES",""},
+                    {"CURRENT","false"}
+                };
 
         private void Jobs_Load(object sender, EventArgs e)
         {
@@ -29,10 +42,7 @@ namespace ResumeAutomator
 
             if(data.Jobs == null)
             {
-                Dictionary<string, string> newDict = new Dictionary<string, string>()
-                {
-                    {"LISTTITLE","First Job" }
-                };
+                
                 jobBox.Items.Add("First Job");
                 data.Jobs = new List<Dictionary<string, string>>();
                 data.Jobs.Add(newDict);
@@ -115,11 +125,25 @@ namespace ResumeAutomator
             startYear.Value = Int32.Parse(data.Jobs[index]["STARTYEAR"]);
             endMonth.Value = Int32.Parse(data.Jobs[index]["ENDMONTH"]);
             endYear.Value = Int32.Parse(data.Jobs[index]["ENDYEAR"]);
+            locBox.Text = data.Jobs[index]["LOC"];
+            dutiesBox.Text = data.Jobs[index]["DUTIES"];
+            if (data.Jobs[index]["CURRENT"] == "true")
+            {
+                currentCheck.Checked = true;
+            }
+            else
+            {
+                currentCheck.Checked = false;
+            }
+                
         }
 
         private void jobBox_SelectedIndexChanged(object sender, EventArgs e)
         {
             listIndex = jobBox.SelectedIndex;
+            allowUpdate = false;
+            LoadTextFromIndex(listIndex);
+            allowUpdate = true;
         }
 
         private void Jobs_FormClosing(object sender, FormClosingEventArgs e)
@@ -172,6 +196,115 @@ namespace ResumeAutomator
             else
             {
                 data.Jobs[listIndex].Add("ENDYEAR", endYear.Value.ToString());
+            }
+        }
+
+        private void locBox_TextChanged(object sender, EventArgs e)
+        {
+            if (data.Jobs[listIndex].ContainsKey("LOC"))
+            {
+                data.Jobs[listIndex]["LOC"] = locBox.Text;
+            }
+            else
+            {
+                data.Jobs[listIndex].Add("LOC", locBox.Text);
+            }
+        }
+
+        private void dutiesBox_TextChanged(object sender, EventArgs e)
+        {
+            if (data.Jobs[listIndex].ContainsKey("DUTIES"))
+            {
+                data.Jobs[listIndex]["DUTIES"] = dutiesBox.Text;
+            }
+            else
+            {
+                data.Jobs[listIndex].Add("DUTIES", dutiesBox.Text);
+            }
+        }
+
+        private void moveUpBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void moveDownBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            AddJob("New Job");
+        }
+
+        private void delBtn_Click(object sender, EventArgs e)
+        {
+            RemoveJob(listIndex);
+        }
+
+        private void ResetTextBoxes()
+        {
+            titleBox.Text = null;
+            jobTitleBox.Text = null;
+            startMonth.Value = 1;
+            startYear.Value = 2019;
+            endMonth.Value = 1;
+            endYear.Value = 2020;
+            locBox.Text = null;
+            dutiesBox.Text = null;
+        }
+
+        private void AddJob(string name)
+        {
+            allowUpdate = false;
+            data.Jobs.Add(newDict);
+            jobBox.Items.Add(name);
+            listIndex = jobBox.Items.Count - 1;
+            jobBox.SelectedIndex = listIndex;
+            ResetTextBoxes();
+            allowUpdate = true;
+        }
+
+        private void RemoveJob(int index)
+        {
+            allowUpdate = false;
+            data.Jobs.RemoveAt(index);
+            listIndex = 0;
+            jobBox.Items.RemoveAt(index);
+            jobBox.SelectedIndex = listIndex;
+            
+
+
+
+
+            allowUpdate = true;
+        }
+
+        private void currentCheck_CheckedChanged(object sender, EventArgs e)
+        {
+            if (data.Jobs[listIndex].ContainsKey("CURRENT"))
+            {
+                if (currentCheck.Checked)
+                {
+                    data.Jobs[listIndex]["CURRENT"] = "true";
+                }
+                else
+                {
+                    data.Jobs[listIndex]["CURRENT"] = "false";
+                }
+            }
+            else
+            {
+                if (currentCheck.Checked)
+                {
+                    data.Jobs[listIndex].Add("CURRENT", "true");
+                }
+                else
+                {
+                    data.Jobs[listIndex].Add("CURRENT", "false");
+                }
+                
             }
         }
     }
